42b4d9abce10d7a8a8a870f2f4bab880322576cf
Den4ik3090 <grishaevds@yandex.ru>
2025-08-21 17:24:50 +0300

    function

--- function.js
+++ function.js
@@ -1,3 +1,10 @@
+// Функция выполняеся до просмотра всего кода . Работает до того как была вызвана - FUNCTION Declaration
+console.log(calc(20,5));
+console.log(calc(12,13));
+function calc(a,b){
+    return a+b
+}
+
 //Изучение функций 
 let num=20;                            //Переменная указана глобально 
 function showFirstMessage(text){       //Переменный указанные в функции они Локальные и не видны за педелами ф. НО функция может принимать глобальные ПЕРЕМЕННЫЕ 
@@ -25,3 +32,14 @@ function ret(){
 }
 const anotherNum=ret(); //Функция вышла наружу и результат ее отобразится в CONST , и consol log выведет его 
 console.log(anotherNum);
+
+
+//Функция срабатывает только когда до нее дойдет код ( можно вызвать только после обьявления) Function Expression
+const logger=function(){
+console.log('Hello')
+};
+logger();
+
+//Стандарт ES6 стрелочные функции 
+const calc=(a,b)=> a+b; //Такая функция не имеет контекста ВЫЗОВА
+
