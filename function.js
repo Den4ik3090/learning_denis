01c9742b6bc74ba126621a054423b79250322d3a
Den4ik3090 <grishaevds@yandex.ru>
2025-08-21 17:21:59 +0300

    function

---
+++ function.js
@@ -0,0 +1,27 @@
+//Изучение функций 
+let num=20;                            //Переменная указана глобально 
+function showFirstMessage(text){       //Переменный указанные в функции они Локальные и не видны за педелами ф. НО функция может принимать глобальные ПЕРЕМЕННЫЕ 
+    console.log(text)
+    let num=30;
+    console.log(num)
+}
+showFirstMessage('Hello');
+console.log(num)
+
+//
+function calc(a,b){
+    return a+b
+}
+
+console.log(calc(10,10));
+console.log(calc(13,10));
+console.log(calc(20,5));
+console.log(calc(12,13));
+
+//
+function ret(){
+    let num=50;
+    return num;
+}
+const anotherNum=ret(); //Функция вышла наружу и результат ее отобразится в CONST , и consol log выведет его 
+console.log(anotherNum);
